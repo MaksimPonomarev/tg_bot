42ff19d145d1c66a39734dbbd3a6d367c7b6675c
MaksimPonomarev <ponomarevmaksim989@gmail.com>
2024-10-05 04:19:00 +0300

    Добавил файлы проекта

---
+++ config.py
@@ -0,0 +1,7 @@
+import telebot
+
+TOKEN = "6924875174:AAFeI-u0x7mHIGGGqK9pj7G1V4OADFLscic"
+bot = telebot.TeleBot(TOKEN)
+VK = "https://vk.com/i_am_maks"
+GIT = "https://github.com/MaksimPonomarev"
+LINKEDIN = "https://www.linkedin.com/in/maksim-ponomarev-433bb12b4/"
